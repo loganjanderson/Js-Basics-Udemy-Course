6da3e47816c46d08a99c70c28986e45a86039682
Logan Anderson <logananderson@Logans-MBP.T-mobile.com>
2021-03-17 08:25:58 -0600

    Numbers Practice

---
+++ numbers.js
@@ -0,0 +1,13 @@
+// let age = 25
+// let dogYears = (age +1) / 7;
+
+// console.log(dogYears);
+
+// Student Score Calculator
+
+let score = 18
+let maxScore = 25
+
+let percent = (score / maxScore) * 100;
+
+console.log(percent + '%');
