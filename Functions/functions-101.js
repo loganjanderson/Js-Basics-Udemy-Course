299c8c3429b616e0e8c46669cd8dfe71a3c7f883
Logan Anderson <logananderson@Logans-MBP.T-mobile.com>
2021-03-26 20:40:53 -0600

    added folders and start functions

---
+++ Functions/functions-101.js
@@ -0,0 +1,9 @@
+// First Functions Challenge
+
+const convertFahrenheitToCelcius = function (temp) {
+  let celcius = ((temp - 32) * 5) / 9;
+  console.log(celcius);
+};
+
+convertFahrenheitToCelcius(32);
+convertFahrenheitToCelcius(68);
